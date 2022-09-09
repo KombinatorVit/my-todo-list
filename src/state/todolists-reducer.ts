3e407bcd9552c468ad7e55ff300bd5ea418ea473
Vit Onischenko <slonik0775@gmail.com>
2022-09-09 20:16:08 +0300

    08 - Вторник 4. todolists-reducer + AddTodolist

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,4 +1,5 @@
 import {TodolistsType} from '../App';
+import {v1} from 'uuid';
 
 type ActionType = {
     type: string
@@ -10,13 +11,30 @@ type ActionType = {
 // согласно прописанному type в этом action (инструкции) я поменяю state
 export const todolistsReducer = (state: Array<TodolistsType>, action: ActionType) => {
     switch (action.type) {
-        case 'REMOVE-TODOLIST':
- {
-       return  state.filter(t=>t.id !== action.id)
-}
+        case 'REMOVE-TODOLIST': {
 
+            return state.filter(t => t.id !== action.id);
+        }
+        case 'ADD-TODOLIST': {
+         return   [
+                ...state, {
+            id: v1(), title: action.title, filter: 'all'
+            }
+            ]
+
+        }
+        // function addTodolist(title: string) {
+        //     let newTodolistId = v1();
+        //     let newTodolist: TodolistsType = {id: newTodolistId, title, filter: 'all'};
+        //     setTodolists([newTodolist, ...todolists]);
+        //     setTasks({
+        //         ...tasks,
+        //         [newTodolistId]: []
+        //     });
+        // }
 
         default:
-            throw new Error('I don\'t understand this type')
+            throw new Error('I don\'t understand this type');
     }
-}
+};
+
