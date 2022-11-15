8128b3901fc7bd12af455809880999d80db9ca5a
Vit Onischenko <slonik0775@gmail.com>
2022-11-15 17:21:29 +0200

    Progressbar and error handling (part 1)

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,27 @@
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+export type ErrorType = null | string
+const initialState = {
+    status: 'loading' as RequestStatusType,
+    error: null as ErrorType
+};
+
+type InitialStateType = typeof initialState
+
+export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status};
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
+        default:
+            return state;
+    }
+};
+
+export const setErrorAC = (error: ErrorType) => ({type:'APP/SET-ERROR', error} as const)
+export const setStatusAC = (status: RequestStatusType) => ({type:'APP/SET-STATUS', status} as const)
+
+export type SetErrorActionType = ReturnType<typeof setErrorAC>;
+export type SetStatusActionType = ReturnType<typeof setStatusAC>;
+type ActionsType = SetErrorActionType |
+    SetStatusActionType
